72bb499e8be85db1b60339b8eed22efbf61b6e55
Etienne <etienne.burdet@gmail.com>
2019-11-14 12:05:28 +0100

    ingredient seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Reseting all ingredients"
+Ingredient.destroy_all
+
+ingredients_serial = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients = JSON.parse(ingredients_serial)
+
+ingredients["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.create!(name: ingredient["strIngredient1"])
+  puts "Added #{new_ingredient.name}"
+end
